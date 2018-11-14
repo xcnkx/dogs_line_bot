cf7c766bc8b5f299a88e51da253eb2562eb14adc
xcnkx <cnk_2806@me.com>
2018-11-15 00:12:24 +0900

    uploaded

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 import sys
-from flask import Flask, request, abort
+from io import BytesIO
+from keras.models import load_model
+from keras.preprocessing.image import load_img, img_to_array
 
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -9,11 +12,11 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage
 )
 
 app = Flask(__name__)
-
+file_path = "/images"
 # 環境変数からchannel_secret・channel_access_tokenを取得
 channel_secret = os.environ['CHANNEL_SECRET']
 channel_access_token = os.environ['CHANNEL_ACCESS_TOKEN']
@@ -28,9 +31,23 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+model = load_model("model/model_inceptv3_OSME_SE_miru_dogs.best_loss.hdf5")
+classes = ['Chihuahua', 'Japanese_spaniel', 'Maltese_dog', 'Pekinese', 'Shih', 'Blenheim_spaniel', 'papillon', 'toy_terrier', 'Rhodesian_ridgeback', 'Afghan_hound', 'basset', 'beagle', 'bloodhound', 'bluetick', 'black', 'Walker_hound', 'English_foxhound', 'redbone', 'borzoi', 'Irish_wolfhound', 'Italian_greyhound', 'whippet', 'Ibizan_hound', 'Norwegian_elkhound', 'otterhound', 'Saluki', 'Scottish_deerhound', 'Weimaraner', 'Staffordshire_bullterrier', 'American_Staffordshire_terrier', 'Bedlington_terrier', 'Border_terrier', 'Kerry_blue_terrier', 'Irish_terrier', 'Norfolk_terrier', 'Norwich_terrier', 'Yorkshire_terrier', 'wire', 'Lakeland_terrier', 'Sealyham_terrier', 'Airedale', 'cairn', 'Australian_terrier', 'Dandie_Dinmont', 'Boston_bull', 'miniature_schnauzer', 'giant_schnauzer', 'standard_schnauzer', 'Scotch_terrier', 'Tibetan_terrier', 'silky_terrier', 'soft', 'West_Highland_white_terrier', 'Lhasa', 'flat', 'curly', 'golden_retriever', 'Labrador_retriever', 'Chesapeake_Bay_retriever', 'German_short', 'vizsla', 'English_setter', 'Irish_setter', 'Gordon_setter', 'Brittany_spaniel', 'clumber', 'English_springer', 'Welsh_springer_spaniel', 'cocker_spaniel', 'Sussex_spaniel', 'Irish_water_spaniel', 'kuvasz', 'schipperke', 'groenendael', 'malinois', 'briard', 'kelpie', 'komondor', 'Old_English_sheepdog', 'Shetland_sheepdog', 'collie', 'Border_collie', 'Bouvier_des_Flandres', 'Rottweiler', 'German_shepherd', 'Doberman', 'miniature_pinscher', 'Greater_Swiss_Mountain_dog', 'Bernese_mountain_dog', 'Appenzeller', 'EntleBucher', 'boxer', 'bull_mastiff', 'Tibetan_mastiff', 'French_bulldog', 'Great_Dane', 'Saint_Bernard', 'Eskimo_dog', 'malamute', 'Siberian_husky', 'affenpinscher', 'basenji', 'pug', 'Leonberg', 'Newfoundland', 'Great_Pyrenees', 'Samoyed', 'Pomeranian', 'chow', 'keeshond', 'Brabancon_griffon', 'Pembroke', 'Cardigan', 'toy_poodle', 'miniature_poodle', 'standard_poodle', 'Mexican_hairless', 'dingo', 'dhole', 'African_hunting_dog']
+
+
+def predict(image):
+    img = load_img(image)
+    x = img_to_array(img, target_size=(448,448))
+    predicts = model.predict(x.reshape([-1,448,448,3]))
+    top3 = predicts.argsort()[2::-1]
+    top3_acc = predicts.sort()[2::-1]
+    return top3, top3_acc
+
+
 @app.route("/")
 def hello_world():
     return "hello world!"
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -49,11 +66,38 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # print("handle_message:", event)
+    text = event.message.text
+
+    messages = [
+        TextSendMessage(text=text),
+        TextSendMessage(text='犬の画像を送ってみて！品種当てちゃうぞ！'),
+    ]
+
+    line_bot_api.reply_message(event, messages)
+
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_message(event):
+    message_id = event.message.id
+    message_content = line_bot_api.get_message_content(message_id)
+
+    image = BytesIO(message_content.content)
+    top3_index, top3_acc = predict(image)
+
+    message = "This dog is:\n" + classes[top3_index[0]] + "  acc."+ str(float(top3_acc[0])) + "\n" + \
+              classes[top3_index[1]] + "  acc." + str(float(top3_acc[1])) + "\n" +classes[top3_index[2]] + \
+              "  acc." + str(float(top3_acc[2]))
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="がんばれ！！七海！！大好きだよ"))
+        TextSendMessage(
+            text=message
+        )
+    )
+
 
 if __name__ == "__main__":
     app.run()
